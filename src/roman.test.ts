bbfa7645181db46e44789bc1ee443e75446e5927
Miguel Gonzales <miguel.gonzales@assuresoft.com>
2026-05-29 10:21:34 -0400

    feature: add several tests with breaking rules. All green, coverage 100%.

--- src/roman.test.ts
+++ src/roman.test.ts
@@ -151,4 +151,40 @@ describe("fromRoman", () => {
   it("converts roman number L to number 50", () => {
     expect(fromRoman("L")).toBe(50)
   });
+
+  it("converts roman number XC to number 90", () => {
+    expect(fromRoman("XC")).toBe(90)
+  });
+
+  it("converts roman number XCIX to number 99", () => {
+    expect(fromRoman("XCIX")).toBe(99)
+  })
+
+  it("converts roman number CD to number 400", () => {
+    expect(fromRoman("CD")).toBe(400)
+  });
+
+  it("converts roman number CM to number 900", () => {
+    expect(fromRoman("CM")).toBe(900)
+  })
+
+  it("converts roman number CMXL to number 940", () => {
+    expect(fromRoman("CMXL")).toBe(940)
+  })
+
+  it("converts roman number MCMXCIX to number 1999", () => {
+    expect(fromRoman("MCMXCIX")).toBe(1999)
+  });
+
+  it("converts roman number MCMLXXXVII to number 1987", () => {
+    expect(fromRoman("MCMLXXXVII")).toBe(1987)
+  });
+
+  it("converts roman number 2024 to number MMXXIV", () => {
+    expect(fromRoman("MMXXIV")).toBe(2024)
+  });
+
+  it("converts roman number MMMCMXCIX to number 3999", () => {
+    expect(fromRoman("MMMCMXCIX")).toBe(3999)
+  });
 });
